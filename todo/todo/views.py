8bc63e6f406fc80ec5c7572be0b6d86a1a5ae349
ft-mammoo <mammuofficial007@gmail.com>
2025-09-19 10:58:35 +0530

    Signup Completed

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import ToDo
 
 def signup(request):
+
+    if request.method == 'POST':
+        fnm = request.POST.get('fnm')
+        emailid = request.POST.get('emailid')
+        pwd = request.POST.get('pwd')
+        print(fnm, emailid, pwd)
+        new_user = User.objects.create_user(fnm, emailid, pwd)
+        new_user.save()
+        return redirect('/login')
     return render(request, 'signup.html')
+
