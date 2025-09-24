de6bc60bac9e8b72267cc3dfd5ddccad62063edc
ft-mammoo <mammuofficial007@gmail.com>
2025-09-24 12:37:31 +0530

    Made Home Link Effective

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
+    path('', lambda request: redirect('todo/')),
     path('admin/', admin.site.urls),
     path('signup/', views.signup),
     path('login/', views.login),
